ee276ba093f1e157fddc783e8388cd8cbc400183
Stefan Westerfeld <stefan@space.twc.de>
2024-06-29 19:16:28 +0200

    smscript should always be in script mode
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- gen-wavs.sh
+++ gen-wavs.sh
@@ -33,7 +33,9 @@ do
   pho=$(echo $xml|sed s/.xml$/.pho/g)
   script=$(echo $xml|sed s/.xml$/.script/g)
   wav=$(echo $xml|sed s/.xml$/.wav/g)
+  rm -f $pho $script $wav
+
   ./xml-to-pho.py xml testxml/$xml > pho/$pho || echo "$xml -> $pho" failed
   phomorphdi.py pho/$pho > script/$script || echo "$pho -> $script" failed
-  src/smscript script voice/sven.smplan script/$script | ascii2wav -r 48000 wav/$wav
+  src/smscript voice/sven.smplan script/$script | ascii2wav -r 48000 wav/$wav
 done
--- src/smscript.cc
+++ src/smscript.cc
@@ -17,18 +17,16 @@ int
 main (int argc, char **argv)
 {
   Main main (&argc, &argv);
-  bool script_mode = (argc == 4) && (strcmp (argv[1], "script") == 0);
-  if (argc != 3 && !script_mode)
+  if (argc != 3)
     {
-      fprintf (stderr, "usage: testmidisynth <plan> <note>\n");
-      fprintf (stderr, "usage: testmidisynth script <plan> <script>\n");
+      fprintf (stderr, "usage: smscript <plan> <script>\n");
       return 1;
     }
 
   Project project;
   project.set_mix_freq (48000);
 
-  Error error = project.load (script_mode ? argv[2] : argv[1]);
+  Error error = project.load (argv[1]);
   assert (!error);
 
   for (MorphOperator *op : project.morph_plan()->operators())
@@ -44,70 +42,46 @@ main (int argc, char **argv)
 
   MidiSynth& midi_synth = *project.midi_synth();
 
-  if (script_mode)
+  MicroConf script_parser (argv[2]);
+  if (!script_parser.open_ok())
     {
-      MicroConf script_parser (argv[3]);
-      if (!script_parser.open_ok())
-        {
-          fprintf (stderr, "error opening file %s\n", argv[3]);
-          exit (1);
-        }
-      script_parser.set_number_format (MicroConf::NO_I18N);
-
-      while (script_parser.next())
-        {
-          int i, j, ch;
-          double d;
-
-          if (script_parser.command ("note_on", ch, i, j))
-            {
-              unsigned char note_on[3] = { uint8 (0x90 + ch), uint8 (i), uint8 (j) };
-              midi_synth.add_midi_event (0, note_on);
-            }
-          else if (script_parser.command ("note_off", ch, i, j))
-            {
-              unsigned char note_off[3] = { uint8 (0x80 + ch), uint8 (i), uint8 (j) };
-              midi_synth.add_midi_event (0, note_off);
-            }
-          else if (script_parser.command ("process", i))
-            {
-              vector<float> output (i);
-              midi_synth.process (output.data(), i);
-              for (auto f: output)
-                sm_printf ("%.17g\n", f);
-            }
-          else if (script_parser.command ("control", i, d))
-            {
-              midi_synth.set_control_input (i, d);
-            }
-          else if (script_parser.command ("pitch_expression", ch, i, d))
-            {
-              midi_synth.add_pitch_expression_event (0, d, ch, i);
-            }
-          else
-            {
-              script_parser.die_if_unknown();
-            }
-        }
+      fprintf (stderr, "error opening file %s\n", argv[2]);
+      exit (1);
     }
-  else
+  script_parser.set_number_format (MicroConf::NO_I18N);
+  while (script_parser.next())
     {
-      const unsigned char note = atoi (argv[2]);
-      vector<float> output (24000);
+      int i, j, ch;
+      double d;
 
-      unsigned char note_on[3] = { 0x90, note, 100 };
-      midi_synth.add_midi_event (0, note_on);
-      midi_synth.process (output.data(), output.size());
-      for (auto f: output)
-        sm_printf ("%.17g\n", f);
-
-      unsigned char note_off[3] = { 0x80, note, 0 };
-      midi_synth.add_midi_event (0, note_off);
-      while (midi_synth.active_voice_count() > 0)
+      if (script_parser.command ("note_on", ch, i, j))
         {
-          midi_synth.process (output.data(), output.size());
+          unsigned char note_on[3] = { uint8 (0x90 + ch), uint8 (i), uint8 (j) };
+          midi_synth.add_midi_event (0, note_on);
+        }
+      else if (script_parser.command ("note_off", ch, i, j))
+        {
+          unsigned char note_off[3] = { uint8 (0x80 + ch), uint8 (i), uint8 (j) };
+          midi_synth.add_midi_event (0, note_off);
+        }
+      else if (script_parser.command ("process", i))
+        {
+          vector<float> output (i);
+          midi_synth.process (output.data(), i);
           for (auto f: output)
             sm_printf ("%.17g\n", f);
+        }
+      else if (script_parser.command ("control", i, d))
+        {
+          midi_synth.set_control_input (i, d);
+        }
+      else if (script_parser.command ("pitch_expression", ch, i, d))
+        {
+          midi_synth.add_pitch_expression_event (0, d, ch, i);
+        }
+      else
+        {
+          script_parser.die_if_unknown();
         }
     }
 }
