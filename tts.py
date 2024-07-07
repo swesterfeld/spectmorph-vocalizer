fd4a1ca18e7f4d56effd716a6eb7f17d4ad88fd4
Stefan Westerfeld <stefan@space.twc.de>
2024-07-07 15:29:32 +0200

    tts.py: use mbrola phone output of espeak-ng
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tts.py
+++ tts.py
@@ -2,21 +2,27 @@
 import subprocess
 import sys
 
+def is_v (v):
+  for vv in [ 'a', 'i', 'I', 'e', 'o', 'O', 'U', '6', '@', 'E' ]:
+    if v == vv or v == vv + ':':
+      return True
+  return v == '_'
+
 freq = 100
 print ("_ 50")
-pho = subprocess.run (["espeak-ng", "-v", "de", sys.argv[1], "-x", "-q" ], capture_output=True, text=True)
-print (pho.stdout, file=sys.stderr)
+pho = subprocess.run (["espeak-ng", "-v", "mb-de2", sys.argv[1], "--pho", "-q" ], capture_output=True, text=True)
 print (";;; VOLUME 0.85")
-for p in pho.stdout:
-  if p != ":" and p != "'" and p != "," and p != "|" and p != "!":
-    if p == " " or p == "\n":
-      print ("_ 1000")
-    else:
-      if p == "A":
-        p = "a"
-      if p == "a" or p == "o" or p == "U":
-        print ("%s 350 0 %f 350 %f" % (p, freq, freq))
+for p in pho.stdout.splitlines():
+  s = p.split ('\t')
+  if len (s) > 0:
+    for x in s[0]:
+      if x == "_":
+        print ("_ 1000")
+      elif x == ":":
+        pass
+      elif is_v(x):
+        print (x,"350 0 80 100 80")
       else:
-        print (p, "50")
+        print (x, "50")
 print (";;; VOLUME 0.85")
 print ("_ 50")
