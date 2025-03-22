feadee89bf9475401762edf53b2413772fac356f
Stefan Westerfeld <stefan@space.twc.de>
2025-03-22 18:00:08 +0100

    tata.py: improve sound quality for speed test
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tata.py
+++ tata.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
 
-# tata.py $(soxi -D voice/sven.flac) 1 > tata.script
+# tata.py $(soxi -D voice/sven.flac) 1 ta > tata.script
 # src/smscript voice/sven.smplan tata.script tata.wav
-
-#243.503731	243.503731	t
-#243.632267	243.632267	a
-
-
-#264.884632	264.884632	a
-#265.514565	265.514565	t
-
 
 import sys
 
@@ -25,41 +17,52 @@ with open ("plosive.label", "r") as file:
 print ("note_on 0 52 100")
 #ct = 243.503731
 
-text = "ta"
-for x in range (len (lines)):
-  tri = lines[x:x+3]
-  if tri[0][1] == "a" and tri[1][1] == text[0] and tri[2][1] == text[0] + "h":
-    #print (tri[0][0], tri[0][1], tri[1][0], tri[1][1], tri[2][0], tri[2][1])
-    ct = (tri[0][0] + tri[1][0]) / 2
-    start = ct
-    end = (tri[1][0] + tri[2][0]) / 2
-  if tri[0][1] == text[0] and tri[1][1] == text[0] + "h" and tri[2][1] == "a":
-    nextt = (tri[0][0] + tri[1][0]) / 2
-    nextend = tri[2][0] + 0.3
-
-phase = 0
-for i in range (10 * 1000):
-  ct += sp / 1000
-  #ct += .3 / 1000
-  '''
-  sp *= 1.001
-  if ct < 264:
-    if ct < 243.632267:
-      ct += 1 / 1000
-    else:
-      ct = 264.884632
+text = sys.argv[3]
+synlist = []
+nlen = 1000
+acc = 0
+for rep in range (40):
+  # S short part
+  # L long part
+  # L * 2 + S * 2 == nlen
+  if nlen < 200:
+    L = nlen / 4
+    S = nlen / 4
   else:
-    ct += sp / 1000
-    if ct > 265.514565:
-      ct = 243.503731
-  sp *= 1.001
-  '''
-  if ct > end and phase == 0:
-    ct = nextt
-    phase = 1
-  if ct > nextend and phase == 1:
-    ct = start
-    phase = 0
+    L = nlen / 2 - 50
+    S = 50
+  for x in range (len (lines)):
+    tri = lines[x:x+3]
+    if tri[0][1] == "a" and tri[1][1] == text[0] and tri[2][1] == text[0] + "h":
+      ct = (tri[0][0] + tri[1][0]) / 2
+      synlist.append ((ct, tri[1][0], L))
+      end = (tri[1][0] + tri[2][0]) / 2
+      synlist.append ((tri[1][0], end, S))
+  for x in range (len (lines)):
+    tri = lines[x:x+3]
+    if tri[0][1] == text[0] and tri[1][1] == text[0] + "h" and tri[2][1] == "a":
+      #nextt = (tri[0][0] + tri[1][0]) / 2
+      synlist.append ((tri[1][0] - 0.02, tri[2][0], S))
+      nextend = tri[2][0] + 0.3
+      synlist.append ((tri[2][0], nextend, L))
+  acc += nlen
+  if acc > 500:
+    nlen *= 0.85
+    acc = 0
+#for s in synlist:
+#  print (s)
+phase = 0
+ms = 0
+ct = synlist[0][0]
+for i in range (1000 * 1000):
+  ratio = (synlist[phase][1] - synlist[phase][0]) * 1000 / synlist[phase][2]
+  print ("#", ratio)
+  ct += sp / 1000 * ratio
+  if ct > synlist[phase][1]:
+    phase += 1
+    if (phase >= len (synlist)):
+      sys.exit (0)
+    ct = synlist[phase][0]
 
   print ("control 0", ct / voice_length * 2 - 1)
   print ("control 1", 0)
