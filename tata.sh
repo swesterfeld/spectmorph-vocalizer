37e6b83f7f0fd81b59f04571aefdddff6cd4e2ae
Stefan Westerfeld <stefan@space.twc.de>
2025-04-09 18:03:26 +0200

    tata.sh: support rendering specific test case
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tata.sh
+++ tata.sh
@@ -1,4 +1,12 @@
-for i in ba ga da pa ta ka tak dap
+set -e
+
+if [ "x$1" != x ]; then
+  F="$@"
+else
+  F="ba ga da pa ta ka tak dap"
+fi
+
+for i in $F
 do
   echo -n "$i ..."
   tata.py $(soxi -D voice/sven.flac) 1 $i > tata.script
