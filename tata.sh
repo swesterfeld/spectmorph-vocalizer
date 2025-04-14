84ff3ce44cdb220959728d260d8b4ac92b0809be
Stefan Westerfeld <stefan@space.twc.de>
2025-04-14 15:07:20 +0200

    tata.sh: support PLAY=1
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- tata.sh
+++ tata.sh
@@ -11,5 +11,8 @@ do
   echo -n "$i ..."
   tata.py $(soxi -D voice/sven.flac) 1 $i > tata.script
   src/smscript voice/sven.smplan tata.script ${i}${i}.wav
+  if [ "x$PLAY" != "x" ]; then
+    gst123 ${i}${i}.wav
+  fi
   echo " OK"
 done
